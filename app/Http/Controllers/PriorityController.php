96f21c6aca910b5f47aff67fa040107899b8ca1a
sasetz <56696451a@gmail.com>
2022-07-10 13:26:51 +0200

    Built priority REST resource

--- app/Http/Controllers/PriorityController.php
+++ app/Http/Controllers/PriorityController.php
@@ -4,62 +4,84 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StorePriorityRequest;
 use App\Http\Requests\UpdatePriorityRequest;
+use App\Http\Resources\PriorityResource;
 use App\Models\Priority;
+use App\Models\Project;
+use App\Models\User;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Http\Response;
+use Throwable;
 
 class PriorityController extends Controller
 {
+    public function __construct()
+    {
+        $this->authorizeResource(Priority::class, 'priority');
+    }
+
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @param Project $project
+     * @return AnonymousResourceCollection|Response
      */
-    public function index()
+    public function index(Project $project): Response|AnonymousResourceCollection
     {
-        //
+        return PriorityResource::collection($project->priorities);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \App\Http\Requests\StorePriorityRequest  $request
-     * @return \Illuminate\Http\Response
+     * @param StorePriorityRequest $request
+     * @return Response
      */
-    public function store(StorePriorityRequest $request)
+    public function store(StorePriorityRequest $request): Response
     {
-        //
+        $priority = new Priority();
+        $priority->project()->associate(Project::find($request->input('project_id')));
+        $priority->fill($request->all());
+        $priority->save();
+        
+        return response('OK');
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Priority  $priority
-     * @return \Illuminate\Http\Response
+     * @param Priority $priority
+     * @return PriorityResource|Response
      */
-    public function show(Priority $priority)
+    public function show(Priority $priority): Response|PriorityResource
     {
-        //
+        return new PriorityResource($priority);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \App\Http\Requests\UpdatePriorityRequest  $request
-     * @param  \App\Models\Priority  $priority
-     * @return \Illuminate\Http\Response
+     * @param UpdatePriorityRequest $request
+     * @param Priority $priority
+     * @return Response
      */
-    public function update(UpdatePriorityRequest $request, Priority $priority)
+    public function update(UpdatePriorityRequest $request, Priority $priority): Response
     {
-        //
+        $priority->fill($request->all());
+        $priority->save();
+        
+        return response('OK');
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Priority  $priority
-     * @return \Illuminate\Http\Response
+     * @param Priority $priority
+     * @return Response
+     * @throws Throwable
      */
-    public function destroy(Priority $priority)
+    public function destroy(Priority $priority): Response
     {
-        //
+        $priority->deleteOrFail();
+        return response('OK');
     }
 }
--- app/Policies/PriorityPolicy.php
+++ app/Policies/PriorityPolicy.php
@@ -3,8 +3,10 @@
 namespace App\Policies;
 
 use App\Models\Priority;
+use App\Models\Project;
 use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Auth\Access\Response;
 
 class PriorityPolicy
 {
@@ -13,82 +15,82 @@ class PriorityPolicy
     /**
      * Determine whether the user can view any models.
      *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @param User $user
+     * @return Response|bool
      */
-    public function viewAny(User $user)
+    public function viewAny(User $user, Project $project): Response|bool
     {
-        //
+        return $user->isAdded($project);
     }
 
     /**
      * Determine whether the user can view the model.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Priority  $priority
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @param User $user
+     * @param Priority $priority
+     * @return Response|bool
      */
-    public function view(User $user, Priority $priority)
+    public function view(User $user, Priority $priority): Response|bool
     {
-        //
+        return $user->isAdded($priority->project);
     }
 
     /**
      * Determine whether the user can create models.
      *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @param User $user
+     * @return Response|bool
      */
-    public function create(User $user)
+    public function create(User $user, Project $project): Response|bool
     {
-        //
+        return $user->isAdmin($project);
     }
 
     /**
      * Determine whether the user can update the model.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Priority  $priority
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @param User $user
+     * @param Priority $priority
+     * @return Response|bool
      */
-    public function update(User $user, Priority $priority)
+    public function update(User $user, Priority $priority): Response|bool
     {
-        //
+        return $user->isAdmin($priority->project);
     }
 
     /**
      * Determine whether the user can delete the model.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Priority  $priority
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @param User $user
+     * @param Priority $priority
+     * @return Response|bool
      */
-    public function delete(User $user, Priority $priority)
+    public function delete(User $user, Priority $priority): Response|bool
     {
-        //
+        return $user->isAdmin($priority->project);
     }
 
     /**
      * Determine whether the user can restore the model.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Priority  $priority
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @param User $user
+     * @param Priority $priority
+     * @return Response|bool
      */
-    public function restore(User $user, Priority $priority)
+    public function restore(User $user, Priority $priority): Response|bool
     {
-        //
+        return $user->isAdmin($priority->project);
     }
 
     /**
      * Determine whether the user can permanently delete the model.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Priority  $priority
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @param User $user
+     * @param Priority $priority
+     * @return Response|bool
      */
-    public function forceDelete(User $user, Priority $priority)
+    public function forceDelete(User $user, Priority $priority): Response|bool
     {
-        //
+        return $user->isAdmin($priority->project);
     }
 }
